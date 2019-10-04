1b3e6428a44f554a9916535e7cf59a5e57ea2960
Ovi Savescu <ovi@ovswebsites.com>
2019-10-04 13:13:57 +0300

    added slugs to categories

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -9,9 +9,21 @@ export default {
       title: 'Title'
     },
     {
-      name: 'description',
-      type: 'text',
-      title: 'Description'
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'This defines the slug for this category\'s page in the front-end.',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule =>
+        Rule.required()
     }
+    // {
+    //   name: 'description',
+    //   type: 'text',
+    //   title: 'Description'
+    // }
   ]
 }
