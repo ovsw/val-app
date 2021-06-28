3fe4acc3e1dfdddf7c0b11897a14c87b5b1f6cca
Ovi Savescu <ovi@ovswebsites.com>
2021-06-28 12:57:24 +0300

    added trailing slashes to blog and  category pages

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,4 +1,4 @@
-const {isFuture} = require('date-fns')
+const { isFuture } = require("date-fns");
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -7,13 +7,11 @@ const {isFuture} = require('date-fns')
 
 // const {format} = require('date-fns')
 
-async function createBlogPages (graphql, actions, reporter) {
-  const {createPage} = actions
+async function createBlogPages(graphql, actions, reporter) {
+  const { createPage } = actions;
   const result = await graphql(`
     {
-      allSanityPost(
-        filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
-      ) {
+      allSanityPost(filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }) {
         edges {
           node {
             id
@@ -31,7 +29,7 @@ async function createBlogPages (graphql, actions, reporter) {
           }
         }
       }
-      allSanityCategory{
+      allSanityCategory {
         edges {
           node {
             id
@@ -42,108 +40,109 @@ async function createBlogPages (graphql, actions, reporter) {
         }
       }
     }
-  `)
+  `);
 
-  if (result.errors) throw result.errors
+  if (result.errors) throw result.errors;
 
-  const postEdges = (result.data.allSanityPost || {}).edges || []
-  const categoryEdges = (result.data.allSanityCategory || {}).edges || []
+  const postEdges = (result.data.allSanityPost || {}).edges || [];
+  const categoryEdges = (result.data.allSanityCategory || {}).edges || [];
 
   // create blog post pages
   postEdges
-    .filter(edge => !isFuture(edge.node.publishedAt))
+    .filter((edge) => !isFuture(edge.node.publishedAt))
     .forEach((edge, index) => {
-      const {id, slug = {}} = edge.node // publishedAt
+      const { id, slug = {} } = edge.node; // publishedAt
       // const dateSegment = format(publishedAt, 'YYYY/MM')
       // const path = `/blog/${dateSegment}/${slug.current}/`
-      const path = `/${slug.current}/`
+      const path = `/${slug.current}/`;
 
-      reporter.info(`Creating blog post page: ${path}`)
+      reporter.info(`Creating blog post page: ${path}`);
 
       createPage({
         path,
-        component: require.resolve('./src/templates/blog-post.js'),
-        context: {id}
-      })
-    })
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: { id },
+      });
+    });
 
   // create blog pagination pages
-  const postsPerPage = 10
-  const numPages = Math.ceil(postEdges.length / postsPerPage)
+  const postsPerPage = 10;
+  const numPages = Math.ceil(postEdges.length / postsPerPage);
 
-  Array.from({length: numPages}).forEach((_, i) => {
-    const path = i === 0 ? `/blog` : `/blog/${i + 1}`
-    reporter.info(`Creating blog post page: ${path}`)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    const path = i === 0 ? `/blog/` : `/blog/${i + 1}/`;
+    reporter.info(`Creating blog post page: ${path}`);
 
     createPage({
       path,
-      component: require.resolve('./src/templates/blog-list-page.js'),
+      component: require.resolve("./src/templates/blog-list-page.js"),
       context: {
         limit: postsPerPage,
         skip: i * postsPerPage,
         numPages,
-        currentPage: i + 1
-      }
-    })
-  })
+        currentPage: i + 1,
+      },
+    });
+  });
 
   // Category Pages with pagination
 
-  let categoriesWithPostsInfo = [] // empty array that will contain objects, each object being a category and its posts
+  let categoriesWithPostsInfo = []; // empty array that will contain objects, each object being a category and its posts
 
-  categoryEdges.map(category => {
-    const currentCatId = category.node.id
-    const currentCatSlug = category.node.slug.current // get the slug of the current category
-    let postsInThisCat = 0 // initialize empty array to hold all the posts in this category
+  categoryEdges.map((category) => {
+    const currentCatId = category.node.id;
+    const currentCatSlug = category.node.slug.current; // get the slug of the current category
+    let postsInThisCat = 0; // initialize empty array to hold all the posts in this category
 
-    postEdges.map(post => { // go through all posts
-      const postCatSlug = post.node.categories[0].slug.current
-      if (postCatSlug === currentCatSlug) { postsInThisCat = postsInThisCat + 1 } // if the post is in this category, add it to postsInThisCat
-    })
+    postEdges.map((post) => {
+      // go through all posts
+      const postCatSlug = post.node.categories[0].slug.current;
+      if (postCatSlug === currentCatSlug) {
+        postsInThisCat = postsInThisCat + 1;
+      } // if the post is in this category, add it to postsInThisCat
+    });
 
     // build the object that represents the category and all its posts
     const categoryWithItsPosts = {
       categoryId: currentCatId,
       categorySlug: currentCatSlug,
-      categoryNumberOfPosts: postsInThisCat
-    }
+      categoryNumberOfPosts: postsInThisCat,
+    };
     // add this category
-    categoriesWithPostsInfo = categoriesWithPostsInfo.concat(categoryWithItsPosts)
-  })
+    categoriesWithPostsInfo = categoriesWithPostsInfo.concat(categoryWithItsPosts);
+  });
 
   // now we have everything we need to create the pages for each category, with pagination
-  categoriesWithPostsInfo.map(({categoryId, categorySlug, categoryNumberOfPosts}) => {
-    const catPostsPerPage = 10
-    const catNumPages = Math.ceil(categoryNumberOfPosts / catPostsPerPage)
+  categoriesWithPostsInfo.map(({ categoryId, categorySlug, categoryNumberOfPosts }) => {
+    const catPostsPerPage = 10;
+    const catNumPages = Math.ceil(categoryNumberOfPosts / catPostsPerPage);
 
     // crete the category's pages (with pagination)
-    Array.from({length: catNumPages}).forEach((_, i) => {
-      const categoryPath = i === 0 ? `/${categorySlug}` : `/${categorySlug}/${i + 1}`
-      reporter.info(`Creating category page: ${categoryPath}`)
+    Array.from({ length: catNumPages }).forEach((_, i) => {
+      const categoryPath = i === 0 ? `/${categorySlug}/` : `/${categorySlug}/${i + 1}/`;
+      reporter.info(`Creating category page: ${categoryPath}`);
 
       createPage({
         path: categoryPath,
-        component: require.resolve('./src/templates/category-page.js'),
+        component: require.resolve("./src/templates/category-page.js"),
         context: {
           limit: catPostsPerPage,
           skip: i * catPostsPerPage,
           catNumPages,
           catCurrentPage: i + 1,
           categoryId,
-          categorySlug
-        }
-      })
-    })
-  })
+          categorySlug,
+        },
+      });
+    });
+  });
 }
 
-async function createGenericPages (graphql, actions, reporter) {
-  const {createPage} = actions
+async function createGenericPages(graphql, actions, reporter) {
+  const { createPage } = actions;
   const result = await graphql(`
     {
-      allSanityPage(
-        filter: { slug: { current: { ne: null } } }
-      ) {
+      allSanityPage(filter: { slug: { current: { ne: null } } }) {
         edges {
           node {
             id
@@ -154,28 +153,27 @@ async function createGenericPages (graphql, actions, reporter) {
         }
       }
     }
-  `)
+  `);
 
-  if (result.errors) throw result.errors
+  if (result.errors) throw result.errors;
 
-  const pageEdges = (result.data.allSanityPage || {}).edges || []
+  const pageEdges = (result.data.allSanityPage || {}).edges || [];
 
-  pageEdges
-    .forEach((edge, index) => {
-      const {id, slug = {}} = edge.node
-      const path = `/${slug.current}/`
+  pageEdges.forEach((edge, index) => {
+    const { id, slug = {} } = edge.node;
+    const path = `/${slug.current}/`;
 
-      reporter.info(`Creating generic page: ${path}`)
+    reporter.info(`Creating generic page: ${path}`);
 
-      createPage({
-        path,
-        component: require.resolve('./src/templates/generic-page.js'),
-        context: {id}
-      })
-    })
+    createPage({
+      path,
+      component: require.resolve("./src/templates/generic-page.js"),
+      context: { id },
+    });
+  });
 }
 
-exports.createPages = async ({graphql, actions, reporter}) => {
-  await createBlogPages(graphql, actions, reporter)
-  await createGenericPages(graphql, actions, reporter)
-}
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  await createBlogPages(graphql, actions, reporter);
+  await createGenericPages(graphql, actions, reporter);
+};
--- web/src/components/pagination/index.js
+++ web/src/components/pagination/index.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import styles from './pagination.module.css'
+import styles from "./pagination.module.css";
 
-const Pagination = ({currentPage, numPages, rootSlug}) => {
+const Pagination = ({ currentPage, numPages, rootSlug }) => {
   // pagination constants
-  const isFirst = currentPage === 1
-  const isLast = currentPage === numPages
-  const prevPage = currentPage - 1 === 1 ? '' : (currentPage - 1).toString()
-  const nextPage = (currentPage + 1).toString()
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage = currentPage - 1 === 1 ? "" : (currentPage - 1).toString();
+  const nextPage = (currentPage + 1).toString();
 
   return (
-    <ul className={`tag-list ${styles.paginationList}`} style={{paddingLeft: '0'}}>
+    <ul className={`tag-list ${styles.paginationList}`} style={{ paddingLeft: "0" }}>
       {!isFirst && (
-        <Link to={`${rootSlug}/${prevPage}`} rel='prev' title='Previous Page'>←</Link>
+        <Link to={`${rootSlug}/${prevPage}/`} rel="prev" title="Previous Page">
+          ←
+        </Link>
       )}
 
-      {Array.from({length: numPages}, (_, i) => (
-        <Link key={`pagination-number${i + 1}`} to={`${rootSlug}/${i === 0 ? '' : i + 1}`} activeClassName={styles.activeLink}>
+      {Array.from({ length: numPages }, (_, i) => (
+        <Link
+          key={`pagination-number${i + 1}`}
+          to={`${rootSlug}/${i === 0 ? "" : i + 1}/`}
+          activeClassName={styles.activeLink}
+        >
           {i + 1}
         </Link>
       ))}
 
       {!isLast && (
-        <Link to={`${rootSlug}/${nextPage}`} rel='next' title='Next Page'>→</Link>
+        <Link to={`${rootSlug}/${nextPage}/`} rel="next" title="Next Page">
+          →
+        </Link>
       )}
     </ul>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
