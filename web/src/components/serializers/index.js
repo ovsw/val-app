000f9c437081483921ffce7308834c813f1d4e83
Ovi Savescu <ovi@ovswebsites.com>
2021-08-04 09:03:34 +0300

    added serializer for internal link

--- web/src/components/serializers/index.js
+++ web/src/components/serializers/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'gatsby'
 import Figure from './Figure'
 import getYouTubeId from 'get-youtube-id'
 import YouTube from 'react-youtube'
@@ -24,6 +25,37 @@ const serializers = {
     },
     mytable: Table,
     iframeEmbed: Embed
+  },
+  marks: {
+    // button: props => {
+    //   const url = props.mark.href
+    //   const isExternal = isAbsoluteURL(url)
+    //   if (isExternal) {
+    //     return <a href={url} rel='noopener noreferrer' target='_blank' className='contentButton'><span>{props.children}<GoLinkExternal /></span></a>
+    //   } else {
+    //     return <Link to={url} activeClassName='active' className='contentButton'><span>{props.children}</span></Link>
+    //   }
+    // },
+    // link: ({mark, children}) => {
+    //   // Read https://css-tricks.com/use-target_blank/
+    //   const {blank, href} = mark
+    //   const isExternal = isAbsoluteURL(href)
+    //   if (isExternal) {
+    //     return <a href={href} className='externalLink' rel='noopener noreferrer' target='_blank'>{children}</a>
+    //     // return blank
+    //     //   ? <a href={href} className='externalLink' rel='noopener noreferrer' target='_blank'>{children}</a>
+    //     //   : <a href={href}>{children}</a>
+    //   } else {
+    //     if (href.charAt(0) === '/') {
+    //       return <a href={`${config.siteUrl}${href}`}>{children}</a>
+    //     } else {
+    //       return <a href={href}>{children}</a>
+    //     }
+    //   }
+    // },
+    internalLink: ({ mark, children }) => {
+      return <Link to={`/${mark.reference.slug.current}/`} >{children}</Link>;
+    }
   }
 }
 
